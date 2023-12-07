e08b5065448a4103c2ca978b8b872f2e5b9713a4
Zeon <89378477+ZeonAnimations@users.noreply.github.com>
2023-12-07 20:43:24 +0530

    git commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// script.js
-
-// Smooth scrolling for anchor links
 $(document).ready(function () {
+    // Smooth scrolling for anchor links
     $('a[href^="#"]').on('click', function (event) {
         event.preventDefault();
 
@@ -13,14 +11,18 @@ $(document).ready(function () {
         }, 900, 'swing', function () {
             window.location.hash = target;
         });
+
+        // Mobile menu close on click
+        if ($('.menu-items').hasClass('show')) {
+            $('.menu-items').removeClass('show');
+        }
     });
-    
+
     // Mobile menu toggle
     $('#mobile-menu').on('click', function () {
         $('.menu-items').toggleClass('show');
     });
 });
-
 // Set the current year in the footer
 const currentYear = new Date().getFullYear();
 document.getElementById('current-year').innerText = currentYear;
